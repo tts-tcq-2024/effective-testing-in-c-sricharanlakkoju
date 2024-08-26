2e04d75137454876edd560eb9fd69ce31accff62
sricharanlakkoju <73473045+sricharanlakkoju@users.noreply.github.com>
2024-08-26 12:10:37 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
@@ -13,9 +14,45 @@ int printColorMap() {
     return i * j;
 }
 
+char capturedOutput[1024];
+char* captureOutput() {
+    capturedOutput[0] = '\0';
+    setvbuf(stdout, capturedOutput, _IOFBF, sizeof(capturedOutput));
+    return capturedOutput;
+}
+
 int main() {
-    int result = printColorMap();
+    captureOutput();
+    int result = printColorMap(); 
+    // Test case to check if the correct color mapping is printed
+    const char* expectedOutput =
+        "0 | White | Blue\n"
+        "1 | White | Orange\n"
+        "2 | White | Green\n"
+        "3 | White | Brown\n"
+        "4 | White | Slate\n"
+        "5 | Red | Blue\n"
+        "6 | Red | Orange\n"
+        "7 | Red | Green\n"
+        "8 | Red | Brown\n"
+        "9 | Red | Slate\n"
+        "10 | Black | Blue\n"
+        "11 | Black | Orange\n"
+        "12 | Black | Green\n"
+        "13 | Black | Brown\n"
+        "14 | Black | Slate\n"
+        "15 | Yellow | Blue\n"
+        "16 | Yellow | Orange\n"
+        "17 | Yellow | Green\n"
+        "18 | Yellow | Brown\n"
+        "19 | Yellow | Slate\n"
+        "20 | Violet | Blue\n"
+        "21 | Violet | Orange\n"
+        "22 | Violet | Green\n"
+        "23 | Violet | Brown\n"
+        "24 | Violet | Slate\n"; 
     assert(result == 25);
+    assert(strcmp(capturedOutput, expectedOutput) == 0);
     printf("All is well (maybe!)\n");
     return 0;
 }
